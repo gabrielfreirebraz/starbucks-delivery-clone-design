d31520947e5e03e092a24edba046c1458e32ef8f
Gabriel Freire Braz <gabrielfreirebraz@gmail.com>
2023-10-08 21:22:09 -0300

    feat: new banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,12 @@
 // import { Button } from '../../components/Button';
 import { HomeContainer } from './styles';
 import imgBanner1 from './../../assets/images/banner/banner_1121600051_desktop.jpg'
+import imgBanner2 from './../../assets/images/banner/banner_195590685_desktop.jpg'
+import imgBanner3 from './../../assets/images/banner/banner_3676615800_desktop.jpg'
+import iconNext from './../../assets/images/chevron-right.svg';
+import iconPrev from './../../assets/images/chevron-left.svg';
+import { Carousel, Col, Row } from 'react-bootstrap';
 import './styles'
-import { Col, Row } from 'react-bootstrap';
 
 
 const Home = () => {
@@ -18,22 +22,39 @@ const Home = () => {
         <Col></Col>
       </Row>
       
-      {/* <h1>My home page</h1>
-      <Button /> */}
-      
     </HomeContainer>
   );
 }
 
+
 const Banner = () => {
-
   return (
-    <>
-      <img src={imgBanner1} />
-    </>
-
+    <Carousel nextIcon={<img src={iconNext} />} prevIcon={<img src={iconPrev} />}>
+      <Carousel.Item>
+        <img src={imgBanner1} alt="Slide 1" />
+        <Carousel.Caption>
+          {/* <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={imgBanner2} alt="Slide 2" />
+        <Carousel.Caption>
+          {/* <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={imgBanner3} alt="Slide 3" />
+        <Carousel.Caption>
+          {/* <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p> */}
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 }
-
 
 export { Home }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,6 +2,22 @@ import styled from "styled-components";
 
 const HomeContainer = styled.div`
   background: transparent;
+
+  .carousel {
+    a.carousel-control-next img,
+    a.carousel-control-prev img {
+      width: 3.5rem !important;
+      background: #f0f0f0;
+      padding: 0.9rem;
+      border-radius: 35px;
+    }
+    /* span.carousel-control-next-icon,
+    span.carousel-control-prev-icon {
+      background: #f0f0f0;
+      border-radius: 25px;
+      padding: 1.5rem;
+    } */
+  }
 `;
 
 export { HomeContainer };
