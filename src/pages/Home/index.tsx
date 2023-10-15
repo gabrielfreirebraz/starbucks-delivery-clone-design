617260721abf11f12979ffb3c1fb2157427c33c1
Gabriel Freire Braz <gabrielfreirebraz@gmail.com>
2023-10-15 18:42:16 -0300

    feat: add subtitles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -98,7 +98,7 @@ const Shop = () => {
               “A cada xícara, a cada conversa, com cada comunidade – Nós nutrimos infinitas possibilidades de conexão humana.”
             </h5>
             <h5>
-              Receba seu cafézinho em casa usando seu rewards
+              Receba seu cafézinho em casa usando seu Starbucks Rewards
             </h5>
           </div>
         </Col>
@@ -106,6 +106,14 @@ const Shop = () => {
 
     
       {/* // ---------------------- LINE 1 */}
+
+
+      <Row>
+        <Col>
+          <div className="sub-title-shop"><div className='traco' /><h5>Lançamentos</h5><div className='traco' /></div>
+        </Col>
+      </Row>
+
 
       <Row>
         
@@ -122,7 +130,7 @@ const Shop = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-cart-plus-fill" viewBox="0 0 16 16">
                   <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
                 </svg>&nbsp;
-                take coffee</ButtonContainer>
+                quero meu café</ButtonContainer>
             </Card.Body>
           </Card>
         </Col>
@@ -139,7 +147,7 @@ const Shop = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-cart-plus-fill" viewBox="0 0 16 16">
                   <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
                 </svg>&nbsp;
-                take coffee</ButtonContainer>
+                quero meu café</ButtonContainer>
             </Card.Body>
           </Card>
         </Col>
@@ -156,7 +164,7 @@ const Shop = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-cart-plus-fill" viewBox="0 0 16 16">
                   <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
                 </svg>&nbsp;
-                take coffee</ButtonContainer>
+                quero meu café</ButtonContainer>
             </Card.Body>
           </Card>
         </Col>
@@ -173,7 +181,7 @@ const Shop = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-cart-plus-fill" viewBox="0 0 16 16">
                   <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
                 </svg>&nbsp;
-                take coffee</ButtonContainer>
+                quero meu café</ButtonContainer>
             </Card.Body>
           </Card>
         </Col>
@@ -191,7 +199,7 @@ const Shop = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-cart-plus-fill" viewBox="0 0 16 16">
                   <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
                 </svg>&nbsp;
-                take coffee</ButtonContainer>
+                quero meu café</ButtonContainer>
             </Card.Body>
           </Card>
         </Col>
@@ -199,6 +207,13 @@ const Shop = () => {
 
 
     {/* // ---------------------- LINE 2 */}
+
+    <Row>
+        <Col>
+          <div className="sub-title-shop"><div className='traco' /><h5>Frappuccinos</h5><div className='traco' /></div>
+        </Col>
+      </Row>
+
 
     <Row>
         <Col>
@@ -213,7 +228,7 @@ const Shop = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-cart-plus-fill" viewBox="0 0 16 16">
                   <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
                 </svg>&nbsp;
-                take coffee</ButtonContainer>
+                quero meu café</ButtonContainer>
             </Card.Body>
           </Card>
         </Col>
@@ -230,7 +245,7 @@ const Shop = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-cart-plus-fill" viewBox="0 0 16 16">
                   <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
                 </svg>&nbsp;
-                take coffee</ButtonContainer>
+                quero meu café</ButtonContainer>
             </Card.Body>
           </Card>
         </Col>
@@ -247,7 +262,7 @@ const Shop = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-cart-plus-fill" viewBox="0 0 16 16">
                   <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
                 </svg>&nbsp;
-                take coffee</ButtonContainer>
+                quero meu café</ButtonContainer>
             </Card.Body>
           </Card>
         </Col>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -28,6 +28,7 @@ const HomeContainer = styled.div`
     flex-direction: column;
     justify-content: center;
     margin-bottom: 2rem;
+    padding: 0 7rem;
   }
   #title-shop h5 {
     color: ${(props) => props.theme.darkGreen};
@@ -36,12 +37,32 @@ const HomeContainer = styled.div`
   #title-shop h5:last-child {
     font-weight: 700 !important;
   }
-
+  .sub-title-shop {
+    text-align: center;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+  .sub-title-shop h5 {
+    color: ${(props) => props.theme.darkGreen};
+    text-transform: uppercase;
+    font-size: 1.2rem;
+    letter-spacing: 0.1rem;
+    font-weight: 700;
+    margin: 3rem 0 2rem;
+  }
+  /* .sub-title-shop .traco {
+    border-bottom: solid 3px ${(props) => props.theme.darkGreen};
+    height: 16px;
+    width: 20px;
+    margin: 0 1rem;
+  } */
   .card {
     margin: 2rem 0 4rem;
   }
   .card svg.bi {
-    margin: 0 0.2rem 0.2rem 0;
+    margin: -0.1rem 0.2rem 0.2rem 0;
     width: 14px;
   }
   .card .card-img-top {
