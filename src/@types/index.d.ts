2f2b35de752b73666b24d3dfa163034c3c04cfc3
Gabriel Freire Braz <gabrielfreirebraz@gmail.com>
2023-10-15 21:38:42 -0300

    feat: add context

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -2,3 +2,5 @@ declare module "*.jpg";
 declare module "*.jpeg";
 declare module "*.png";
 declare module "*.svg";
+
+interface IAppContext {}
--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { ThemeProvider } from "styled-components"
 import { Router } from "./Router"
 import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/global"
+import { AppProvider } from "./contexts/defaultContext"
 
 function App() {
 
   return (    
-    <ThemeProvider theme={defaultTheme}>
-      <Router />
-      <GlobalStyle />
+    <ThemeProvider theme={defaultTheme}>      
+      <AppProvider.Provider value={{}}>
+        <Router />
+        <GlobalStyle />
+      </AppProvider.Provider>
     </ThemeProvider>
     
   )
