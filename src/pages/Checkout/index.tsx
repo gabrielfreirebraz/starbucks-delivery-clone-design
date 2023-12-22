8e8bd8ce91a53f86e5692c1a79845ee0de5cc656
Gabriel Freire Braz <gabrielfreirebraz@gmail.com>
2023-12-22 18:20:44 -0300

    feat: init checkout page with blocks

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { Cart } from "../../components/Cart";
 import { AppProvider } from "../../contexts/defaultContext";
+import { CheckoutContainer } from "./styles";
+import { Col, Row } from "react-bootstrap";
 
 
 
@@ -10,10 +12,46 @@ export const Checkout = () => {
 
   return  (
 
-    <>
-      <h1>Checkout page</h1>
+      <CheckoutContainer>
+        <Row>
+          <div className="title-checkout">
+            <h5>Complete o seu pedido</h5>
+          </div>
+        </Row>
+        <Row>
+          <Col md={2} />
+          <Col md={8}>
+            <div id="checkout">
+              <div className="blocks-1">
+                <div className="block">
+                  <h5>&nbsp;
 
-      <Cart on={cartOn}></Cart>
-    </>
+
+                    {/* “A cada xícara, a cada conversa, com cada comunidade – Nós nutrimos infinitas possibilidades de conexão humana.” */}
+                  </h5>
+                </div>
+
+                <div className="block">
+                  <h5>&nbsp;
+                    {/* “A cada xícara, a cada conversa, com cada comunidade – Nós nutrimos infinitas possibilidades de conexão humana.” */}
+                  </h5>
+                </div>
+              </div>
+
+              <div className="blocks-2">
+                <div className="block">
+                  <h5>&nbsp;
+                    {/* “A cada xícara, a cada conversa, com cada comunidade – Nós nutrimos infinitas possibilidades de conexão humana.” */}
+                  </h5>
+                </div>
+              </div>
+              
+            </div>
+          </Col>
+        </Row>
+
+        <Cart on={cartOn}></Cart>
+      </CheckoutContainer>
+      
   );
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,1 +1,67 @@
+import styled from "styled-components";
 
+export const CheckoutContainer = styled.div`
+  #checkout {
+    /* border: solid 1px #000; */
+    min-height: 30rem;
+    max-height: fit-content;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 2rem;
+    padding: 0;
+    gap: 1rem;
+
+    .blocks-1 {
+      width: 50%;
+      /* border: solid 4px red; */
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+
+      .block:first-child {
+        height: 70%;
+      }
+      .block {
+        background-color: ${(props) => props.theme.secondary};
+        border-radius: 4px;
+        height: 30%;
+        width: 100%;
+      }
+    }
+    .blocks-2 {
+      width: 50%;
+      /* border: solid 4px red; */
+
+      .block {
+        background-color: ${(props) => props.theme.secondary};
+        border-radius: 4px 35px;
+        height: 100%;
+        width: 100%;
+      }
+    }
+  }
+  #checkout h5 {
+    color: ${(props) => props.theme.darkGreen};
+    font-weight: 400;
+  }
+  #checkout h5:last-child {
+    font-weight: 700 !important;
+  }
+
+  .title-checkout {
+    text-align: center;
+    display: flex;
+    flex-direction: row;
+    align-items: left;
+    justify-content: center;
+  }
+  .title-checkout h5 {
+    color: ${(props) => props.theme.darkGreen};
+    text-transform: uppercase;
+    font-size: 1.2rem;
+    letter-spacing: 0.1rem;
+    font-weight: 700;
+    margin: 3rem 0 2rem;
+  }
+`;
