dafd1e9b1ed96889a61cedbe2c0c8d1659ec3a30
Gabriel Freire Braz <gabrielfreirebraz@gmail.com>
2023-12-24 16:29:11 -0300

    feat: cep as data fetch

--- src/pages/Checkout/FormAddress/index.tsx
+++ src/pages/Checkout/FormAddress/index.tsx
@@ -1,25 +1,44 @@
 import { Col, Form, Row } from "react-bootstrap";
 import { FormAddressContainer } from "./styles";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CheckoutProvider } from "../../../contexts/defaultContext";
+import axios from "axios";
 
 
 export const FormAddress = () => {
  
   const { register, formState: { errors } } = useContext(CheckoutProvider);
+  const [rua, setRua] = useState(null);
+  const [bairro, setBairro] = useState(null);
+  const [complemento, setComplemento] = useState(null);
+  const [uf, setUF] = useState(null);
+  const [cidade, setCidade] = useState(null);
   
+  const handleCEP = async (cep: number) => {
+
+    if (cep.toString().length >= 8) {
+      const dataAddress = await axios.get(`http://viacep.com.br/ws/${cep}/json/`);
+
+      setBairro( dataAddress?.data.bairro ?? null );
+      setComplemento( dataAddress?.data.complemento ?? null );
+      setRua( dataAddress?.data.logradouro ?? null );
+      setUF( dataAddress?.data.uf ?? null );
+      setCidade( dataAddress?.data.localidade ?? null );
+    }
+  }
+
   return (
     <FormAddressContainer>
       <Row>
         <Col md={4}>
           <Form.Group className="mb-3" controlId="formBasicCep">
-            <Form.Control type="number" placeholder="CEP" {...register("cep", { required: true })} />
+            <Form.Control type="number" placeholder="CEP" {...register("cep", { required: true, onChange: (e) => handleCEP(e.target.value)  })} />
           </Form.Group>
         </Col>
         
         <Col md={8}>
           <Form.Group className="mb-3" controlId="formBasicRua">
-            <Form.Control type="text" placeholder="Rua" {...register("rua", { required: true })} />
+            <Form.Control type="text" placeholder="Rua" {...register("rua", { required: false })} value={rua} />
           </Form.Group>
         </Col>
       </Row>
@@ -27,13 +46,13 @@ export const FormAddress = () => {
       <Row>
         <Col md={4}>
           <Form.Group className="mb-3" controlId="formBasicNumero">
-            <Form.Control type="text" placeholder="Número" {...register("numero", { required: true })} />
+            <Form.Control type="text" placeholder="Número" {...register("numero", { required: false })}  />
           </Form.Group>
         </Col>
         
         <Col md={8}>
           <Form.Group className="mb-3" controlId="formBasicComplemento">
-            <Form.Control type="text" placeholder="Complemento (Opcional)" {...register("complemento")} />
+            <Form.Control type="text" placeholder="Complemento (Opcional)" {...register("complemento")} value={complemento}  />
           </Form.Group>
         </Col>
       </Row>
@@ -41,19 +60,19 @@ export const FormAddress = () => {
       <Row>
         <Col md={4}>
           <Form.Group className="mb-3" controlId="formBasicBairro">
-            <Form.Control type="text" placeholder="Bairro" {...register("bairro", { required: true })} />
+            <Form.Control type="text" placeholder="Bairro" {...register("bairro", { required: false })} value={bairro}  />
           </Form.Group>
         </Col>
 
         <Col md={6}>
           <Form.Group className="mb-3" controlId="formBasicCidade">
-            <Form.Control type="text" placeholder="Cidade" {...register("cidade", { required: true })} />
+            <Form.Control type="text" placeholder="Cidade" {...register("cidade", { required: false })} value={cidade}  />
           </Form.Group>
         </Col>
 
         <Col md={2}>
           <Form.Group className="mb-3" controlId="formBasicUF">
-            <Form.Control type="text" placeholder="UF" {...register("uf", { required: true })} />
+            <Form.Control type="text" placeholder="UF" {...register("uf", { required: false })} value={uf}  />
           </Form.Group>
         </Col>
 
